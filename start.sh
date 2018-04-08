114fa7e086de68f90332f4dd7a04beb10a1b43bc
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-04-08 17:20:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@
 NODEOSBINDIR="/home/eos-dawn-v3.0.0/eos/build/programs/nodeos"
 DATADIR="/opt/JungleTestnet"
 
-./stop.sh
+$DATADIR/stop.sh
 echo "Starting Nodeos \n";
 
 $NODEOSBINDIR/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
