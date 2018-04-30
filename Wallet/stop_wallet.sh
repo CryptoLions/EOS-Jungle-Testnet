4cf6bf5ad48a0b35f5c7965b64231497d6013466
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-04-30 17:34:35 +0300

    Create stop_wallet.sh

---
+++ Wallet/stop_wallet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet
+#
+###############################################################################
+
+DIR="/opt/JungleTestnet/Wallet"
+
+    if [ -f $DIR"/wallet.pid" ]; then
+        pid=$(cat $DIR"/wallet.pid")
+        echo $pid
+        kill $pid
+        rm -r $DIR"/wallet.pid"
+
+        echo -ne "Stoping Wallet"
+
+        while true; do
+            [ ! -d "/proc/$pid/fd" ] && break
+            echo -ne "."
+            sleep 1
+        done
+        echo -ne "\rWallet stopped. \n"
+
+    fi
+
