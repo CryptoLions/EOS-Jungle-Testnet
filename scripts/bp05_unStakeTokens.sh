06e409d022ee61bda032290f50e38dd5b1cd5a3b
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-05-23 15:36:27 +0300

    Create bp05_unStakeTokens.sh

---
+++ scripts/bp05_unStakeTokens.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# For EOS Junlge testnet
+#
+# https://github.com/CryptoLions/
+#
+################################################################################
+FROM="acryptolions"
+TO="acryptolions"
+NET="1000.0000 EOS"
+CPU="1000.0000 EOS"
+
+./cleos.sh system undelegatebw $FROM $TO $NET $CPU -p $FROM
