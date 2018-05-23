73e0f75b42d8aeb5fb1a066da27e772abda10bd3
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-05-23 15:38:26 +0300

    Create bp06_unRegisterProducer.sh

---
+++ scripts/bp06_unRegisterProducer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# For EOS Junlge testnet
+#
+# https://github.com/CryptoLions/
+#
+################################################################################
+ACCOUNT="acryptolions"
+
+./cleos.sh system unregprod $ACCOUNT -p $ACCOUNT
