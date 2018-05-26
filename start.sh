9abe75fbf0b35ec18e9508c3588ad8fc2fbd1a4d
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-05-26 06:23:33 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,13 +7,10 @@
 ###############################################################################
 
 
-NODEOSBINDIR="/home/eos-dawn-v4.0.0/eos/build/programs/nodeos"
+NODEOSBINDIR="/home/eos-dawn-v4.2.0/eos/build/programs/nodeos"
 DATADIR="/opt/JungleTestnet"
 
 $DATADIR/stop.sh
 echo -e "Starting Nodeos \n";
 
-$NODEOSBINDIR/nodeos --shared-memory-size-mb 4096 --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
-#$NODEOSBINDIR/nodeos --replay $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
-#$NODEOSBINDIR/nodeos --enable-stale-production true $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
-#$EOSIOBINDIR/nodeos --enable-stale-production true --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+$NODEOSBINDIR/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
