2dbd0fae6006bd9fec0a3adfc97ca9482d19a62e
Pluswave <pluswave@xiaofuxing.name>
2018-05-27 22:55:58 +0800

    fix an issue for cmdline args

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ DATADIR="/opt/JungleTestnet"
 $DATADIR/stop.sh
 echo -e "Starting Nodeos \n";
 
-$NODEOSBINDIR/nodeos --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+$NODEOSBINDIR/nodeos --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
