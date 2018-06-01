e6dae85a3b440d3cb38d78b59cdad3d0ac8961e7
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-01 05:46:25 +0300

    Update start_wallet.sh

--- Wallet/start_wallet.sh
+++ Wallet/start_wallet.sh
@@ -7,11 +7,11 @@
 #
 ###############################################################################
 
-NODEOSBINDIR="/home/eos-dawn-v4.0.0/eos/build/programs"
+NODEOSBINDIR="/home/eos-dawn4/eos/build/programs"
 DATADIR="/opt/JungleTestnet/Wallet"
 WALLET_HOST="127.0.0.1"
 WALLET_POSRT="3000"
 
 
 $DATADIR/stop_wallet.sh
-$NODEOSBINDIR/keosd/keosd --data-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
+$NODEOSBINDIR/keosd/keosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
